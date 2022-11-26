eaaacec994cb9a2c36a388ed47591824edbe1c62
Elite Keryakos <elite.keryakos@hotmail.com>
2022-11-26 14:01:56 +0200

    Created SplashScreenActivity and its layout

---
+++ BookEM_client/app/src/main/java/com/edevs/bookem/SplashScreenActivity.java
@@ -0,0 +1,11 @@
+package com.edevs.bookem;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity  extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
