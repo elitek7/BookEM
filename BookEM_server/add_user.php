d10d1f8332d95dc32d587291496eaac052934617
Elite Keryakos <elite.keryakos@hotmail.com>
2022-11-26 17:56:52 +0200

    Created the Add User PHP file

---
+++ BookEM_server/add_user.php
@@ -0,0 +1,13 @@
+<?php
+
+require 'connection.php';
+
+$username = $_POST["username"];
+$password = $_POST["password"];
+$email = $_POST["email"];
+
+$query = $mysqli->prepare("INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (NULL, ?, ?)");
+$query->bind_param("sssssss", $username, $password, $email);
+$query->execute();
+
+echo "Succeeded";
