27666000ebfab06ccef0102afad75e71495ba3c2
Elite Keryakos <elite.keryakos@hotmail.com>
2022-11-26 14:36:48 +0200

    Fully implemented SplashScreenActivity

--- BookEM_client/app/src/main/java/com/edevs/bookem/SplashScreenActivity.java
+++ BookEM_client/app/src/main/java/com/edevs/bookem/SplashScreenActivity.java
@@ -1,11 +1,25 @@
 package com.edevs.bookem;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.content.Intent;
+import java.util.Objects;
 
 public class SplashScreenActivity  extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(() -> {
+
+            Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+            startActivity(intent);
+            finish();
+        }, 5000);
     }
 }
