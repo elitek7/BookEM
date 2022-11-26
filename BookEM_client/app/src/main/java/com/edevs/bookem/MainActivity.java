e908f221ddc200c5b7c5cda409dcd86f21f348dc
Elite Keryakos <elite.keryakos@hotmail.com>
2022-11-26 13:34:17 +0200

    Created Project

---
+++ BookEM_client/app/src/main/java/com/edevs/bookem/MainActivity.java
@@ -0,0 +1,14 @@
+package com.edevs.bookem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
