921bcdc99925b9c1a3a46e7bad60e91dcace3eea
Elite Keryakos <elite.keryakos@hotmail.com>
2022-11-27 22:44:02 +0200

    Created and worked on Reservation abstract class

---
+++ BookEM_client/app/src/main/java/com/edevs/bookem/Reservation.java
@@ -0,0 +1,29 @@
+package com.edevs.bookem;
+
+public abstract class Reservation {
+    protected int reservation_id;
+    protected String start_date;
+    protected String end_date;
+
+    public Reservation(int reservation_id, String start_date, String end_date){
+        this.reservation_id = reservation_id;
+        this.start_date = start_date;
+        this.end_date = end_date;
+    }
+
+    public int getReservationId() {return reservation_id; }
+
+    public void setReservationId(int reservation_id) {
+        this.reservation_id = reservation_id;
+    }
+
+    public String getStartDate() {
+        return start_date;
+    }
+
+    public void setStartDate(String start_date) {this.start_date = start_date;}
+
+    public String getEndDate() {return end_date; }
+
+    public void setEndDate(String end_date) {this.end_date = end_date;}
+}
