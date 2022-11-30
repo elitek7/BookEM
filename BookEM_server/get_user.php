5c2d6b8d49904347d03f4ab94c3fc57e8f29e234
Elite Keryakos <elite.keryakos@hotmail.com>
2022-11-30 14:46:30 +0200

    added get_user API

---
+++ BookEM_server/get_user.php
@@ -0,0 +1,41 @@
+<?php
+
+require 'connection.php';
+
+
+
+if (array_key_exists("user_id", $_GET)) {
+
+    $user_id = $_GET["user_id"];
+    try {
+
+        $query = $mysqli->prepare("SELECT * FROM users WHERE user_id = ?");
+        $query->bind_param("i", $user_id);
+        $query->execute();
+        $result = $query->get_result();
+
+        $row = mysqli_fetch_assoc($result);
+
+        $output["success"] = true;
+        $output["error"] = 0;
+        
+	    $result = [];
+	    $result["user"] = [$row];
+	    $output["query_result"] = $result;
+
+    } catch (Exception $e) {
+
+        $output["success"] = false;
+        $output["query_result"] = null;
+        $output["error"] = $e->getMessage();
+    }
+
+} else {
+
+    $output["success"] = false;
+    $output["query_result"] = null;
+    $output["error"] = "Missing User ID";
+
+}
+
+echo json_encode($output);
