c3dd7dd246c63c9205e2353d4b198eb695435a89
Elite Keryakos <elite.keryakos@hotmail.com>
2022-11-30 12:27:41 +0200

    Created and implemented the Image Encoding class

---
+++ BookEM_client/app/src/main/java/com/edevs/bookem/ImageEncoding.java
@@ -0,0 +1,37 @@
+package com.edevs.bookem;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.util.Base64;
+import android.util.Log;
+
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.channels.FileChannel;
+
+public class ImageEncoding {
+    public static String convertToBase64(Bitmap bitmap) {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG,100,os);
+        byte[] byteArray = os.toByteArray();
+        return Base64.encodeToString(byteArray, 0);
+    }
+
+    public static Bitmap convertToBitmap(String base64String) {
+        if(base64String == null || base64String.length() == 0) return null;
+        byte[] decodedString = Base64.decode(base64String, Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+    }
+    
+    public static Bitmap getBitmapFromXml(Drawable drawable) {
+        Canvas canvas = new Canvas();
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        canvas.setBitmap(bitmap);
+        drawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
+        drawable.draw(canvas);
+
+        return bitmap;
+    }
+}
