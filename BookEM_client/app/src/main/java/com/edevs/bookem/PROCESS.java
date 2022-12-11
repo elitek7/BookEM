1ad4075d168921ceb9ccd4b06d007d860e2cd0aa
Elite Keryakos <elite.keryakos@hotmail.com>
2022-12-12 00:23:29 +0200

    Created the PROCESS interface

---
+++ BookEM_client/app/src/main/java/com/edevs/bookem/PROCESS.java
@@ -0,0 +1,11 @@
+package com.edevs.bookem;
+
+@FunctionalInterface
+public interface PROCESS {
+
+    // An interface to conduit processes through the Linking
+
+    void ACCESS(Constants.Response response);
+
+}
+
