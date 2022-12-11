edaa7e543eafeb52a1e1bf42c529298732891a88
Elite Keryakos <elite.keryakos@hotmail.com>
2022-12-12 01:28:18 +0200

    Worked on the Temp class

--- BookEM_client/app/src/main/java/com/edevs/bookem/Temp.java
+++ BookEM_client/app/src/main/java/com/edevs/bookem/Temp.java
@@ -1,7 +1,16 @@
 package com.edevs.bookem;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.TreeMap;
 
+@RequiresApi(api = Build.VERSION_CODES.N)
 public class Temp {
+    // Holds temporary values
+    public static final TreeMap<Integer, Resource> TEMP_GEMS = new TreeMap<>(Comparator.reverseOrder()); // Retrieved gems
+    public static int TEMP_LATEST_RESOURCE = -1; // Latest resource added
+    public static final HashMap<Integer, User> TEMP_USERS = new HashMap<>(); // Retrieved users
 }
