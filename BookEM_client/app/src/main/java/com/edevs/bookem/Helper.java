25c47b66070b9944fa0b749a17b83412568e5720
Elite Keryakos <elite.keryakos@hotmail.com>
2022-12-12 00:45:51 +0200

    Added the extractUser method

--- BookEM_client/app/src/main/java/com/edevs/bookem/Helper.java
+++ BookEM_client/app/src/main/java/com/edevs/bookem/Helper.java
@@ -25,4 +25,16 @@ public abstract class Helper {
         sp.edit().putString(Constants.Users.USERNAME, user.getUsername()).apply();
         sp.edit().putString(Constants.Users.PASSWORD, user.getPassword()).apply();
     }
+
+    public static User extractUser(Context context) {
+
+        // Retrieves a user from the shared preferences
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+
+        int user_id = sp.getInt(Constants.Users.USER_ID, -1);
+        String username = sp.getString(Constants.Users.USERNAME, "lorem ipsum");
+        String password = sp.getString(Constants.Users.PASSWORD, "lorem ipsum");
+        String email = sp.getString(Constants.Users.EMAIL, "lorem_ipsum@co.com");
+        return new User(user_id, username, password, email);
+    }
 }
