794b15901a20908aa2f2fa870ac9f4bb88d265cc
Elite Keryakos <elite.keryakos@hotmail.com>
2022-12-12 01:21:28 +0200

    Added the Link Class

---
+++ BookEM_client/app/src/main/java/com/edevs/bookem/Link.java
@@ -0,0 +1,28 @@
+package com.edevs.bookem;
+import android.content.Context;
+import android.content.Intent;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Objects;
+public class Link {
+}
