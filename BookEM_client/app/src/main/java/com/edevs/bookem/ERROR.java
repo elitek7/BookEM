2091c1840629a07562412accf4250a2ee3261a3e
Elite Keryakos <elite.keryakos@hotmail.com>
2022-12-12 00:27:30 +0200

    Implemented the ERROR interface

--- BookEM_client/app/src/main/java/com/edevs/bookem/ERROR.java
+++ BookEM_client/app/src/main/java/com/edevs/bookem/ERROR.java
@@ -1,4 +1,9 @@
 package com.edevs.bookem;
 
+@FunctionalInterface
 public interface ERROR {
+
+    // An interface to conduit errors through the Linking
+
+    void DEBUG(String api, Exception e);
 }
