e3b4c585fc467e0ac0896d5e534e22c7aef5214f
Elite Keryakos <elite.keryakos@hotmail.com>
2022-12-11 22:43:40 +0200

    Added a SwipeRefreshLayout to the feed

--- BookEM_client/app/src/main/java/com/edevs/bookem/FeedActivity.java
+++ BookEM_client/app/src/main/java/com/edevs/bookem/FeedActivity.java
@@ -1,4 +1,5 @@
 package com.edevs.bookem;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -8,8 +9,9 @@ import android.view.WindowManager;
 import android.widget.ListView;
 import android.widget.PopupMenu;
 
+
 import androidx.appcompat.app.AppCompatActivity;
-//import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import java.util.ArrayList;
 import java.util.Objects;
@@ -18,7 +20,7 @@ import java.util.Objects;
 public class FeedActivity extends AppCompatActivity {
 
     ListView feed; // The feed holding the gems
-    //SwipeRefreshLayout pullToRefresh;
+    SwipeRefreshLayout pullToRefresh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +34,32 @@ public class FeedActivity extends AppCompatActivity {
 
         // Initializes the feed
         feed = ((ListView) findViewById(R.id.feed));
+
+
+        // Prepares the adapted for the feed
+        ResourcesAdapter adapter = new ResourcesAdapter(FeedActivity.this, feed);
+        feed.setAdapter(adapter);
+
+        // Initializes the layout holding the feed
+        pullToRefresh = findViewById(R.id.pullToRefreshProfile);
+
+        // Retrieves gems from the server an updates the feed once fetches
+        Link.getAllGemsStoreInTempAndUpdateFeed(getApplicationContext(), pullToRefresh, feed, PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getInt(Constants.Users.USER_ID, -1));
+
+        // Adds a refresh functionality to the layout
+        pullToRefresh.setOnRefreshListener(this::onRefresh);
+
     }
+
+
+    public void onRefresh() {
+
+        // Handles the refresh
+        Link.getAllGemsStoreInTempAndUpdateFeed(getApplicationContext(), pullToRefresh, feed, PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getInt(Constants.Users.USER_ID, -1));
+
+    }
+
+
+
+
 }
