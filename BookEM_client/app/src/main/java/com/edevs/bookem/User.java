3d7e4fccce1d8169e221d60b4c0b781ff26b731a
Elite Keryakos <elite.keryakos@hotmail.com>
2022-12-12 00:39:01 +0200

    Added the USer constructor and global variables

--- BookEM_client/app/src/main/java/com/edevs/bookem/User.java
+++ BookEM_client/app/src/main/java/com/edevs/bookem/User.java
@@ -5,4 +5,20 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class User {
+
+    // A class that holds a user
+
+    private int user_id; // The id of the user
+    private String username; // The username of the user
+    private String password; // The password of the user
+    private String email; // The email of the user
+
+    public User(int user_id, String username, String password, String email) {
+
+        // Constructor
+        this.user_id = user_id;
+        this.password = password;
+        this.username = username;
+        this.email = email;
+    }
 }
