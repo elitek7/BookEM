8b34f00140ee80abf6af002bb07ad33d4da07e38
Elite Keryakos <elite.keryakos@hotmail.com>
2022-12-12 00:40:21 +0200

    Added User's accessors and mutators

--- BookEM_client/app/src/main/java/com/edevs/bookem/User.java
+++ BookEM_client/app/src/main/java/com/edevs/bookem/User.java
@@ -21,4 +21,39 @@ public class User {
         this.username = username;
         this.email = email;
     }
+
+    // Accessors and mutators
+    public int getUserId() {
+        return user_id;
+    }
+
+    public void setUserId(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
 }
