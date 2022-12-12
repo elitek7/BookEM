90afcf5baa9eed253127d1fa7177d08e577565fc
Elite Keryakos <elite.keryakos@hotmail.com>
2022-12-12 14:23:14 +0200

    Worked on Link

--- BookEM_client/app/src/main/java/com/edevs/bookem/Temp.java
+++ BookEM_client/app/src/main/java/com/edevs/bookem/Temp.java
@@ -10,7 +10,7 @@ import java.util.TreeMap;
 @RequiresApi(api = Build.VERSION_CODES.N)
 public class Temp {
     // Holds temporary values
-    public static final TreeMap<Integer, Resource> TEMP_GEMS = new TreeMap<>(Comparator.reverseOrder()); // Retrieved gems
+    public static final TreeMap<Integer, Resource> TEMP_RESOURCES = new TreeMap<>(Comparator.reverseOrder()); // Retrieved resources
     public static int TEMP_LATEST_RESOURCE = -1; // Latest resource added
     public static final HashMap<Integer, User> TEMP_USERS = new HashMap<>(); // Retrieved users
 }
