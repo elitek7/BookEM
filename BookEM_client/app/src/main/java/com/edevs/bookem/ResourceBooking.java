14586e8b25340c28c20640fb45a269b413a62f0e
Elite Keryakos <elite.keryakos@hotmail.com>
2022-12-12 23:56:28 +0200

    Calendar attempt

--- BookEM_client/app/src/main/java/com/edevs/bookem/ResourceBooking.java
+++ BookEM_client/app/src/main/java/com/edevs/bookem/ResourceBooking.java
@@ -1,4 +1,82 @@
 package com.edevs.bookem;
 
-public class ResourceBooking {
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import androidx.annotation.VisibleForTesting;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+import java.util.Objects;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.Calendar;
+
+public class ResourceBooking extends AppCompatActivity implements DatePickerDialog.OnDateSetListener , DatePickerDialog.OnDateCancelListener{
+
+    TextView fromDateText;
+    TextView toDateText;
+    Button fromBtn;
+    Button toBtn;
+    SimpleDateFormat simpleDateFormat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        setContentView(R.layout.resource_view);
+
+        fromBtn = (Button) findViewById(R.id.fromBtn);
+        toBtn = (Button) findViewById(R.id.toBtn);
+
+        fromDateText = (TextView) findViewById(R.id.fromDateText);
+        toDateText = (TextView) findViewById(R.id.toDateText);
+        simpleDateFormat = new SimpleDateFormat("dd MM yyyy", Locale.US);
+
+        fromBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDate(1980, 0, 1, R.style.DatePickerSpinner);
+            }
+        });
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        Calendar calendar = new GregorianCalendar(year, monthOfYear, dayOfMonth);
+        dateTextView.setText(simpleDateFormat.format(calendar.getTime()));
+    }
+
+
+
+    @VisibleForTesting
+    void showDate(int year, int monthOfYear, int dayOfMonth, int spinnerTheme) {
+        new SpinnerDatePickerDialogBuilder()
+                .context(MainActivity.this)
+                .callback(MainActivity.this)
+                .onCancel(MainActivity.this)
+                .spinnerTheme(spinnerTheme)
+                .defaultDate(year, monthOfYear, dayOfMonth)
+                .build()
+                .show();
+    }
+
+
+
 }
